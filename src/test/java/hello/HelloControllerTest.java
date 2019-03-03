da1936219fd0578732181d3d247d35bbe14c8b10
simon-towler <simon.towler@icloud.com>
2019-03-03 15:21:14 +0000

    Added Spring annotations to the HelloControllerTest.

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -1,9 +1,14 @@
 package hello;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HelloControllerTest {
 
+    @Autowired
     private MockMvc mvc;
 
+    @Test
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
